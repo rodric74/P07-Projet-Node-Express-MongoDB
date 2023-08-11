78d2f366cd9b8e5aeba1aaa9e80fdb08df0e8d06
rodric74 <rodric.rambosson@gmail.com>
2023-08-11 18:06:19 +0200

    ajout mongoDB + dotenv

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  }).catch(err => {
+    console.error("Error connecting to MongoDB Atlas: ", err.message);
+  });
 
 const signupRoute = require('./routes/auth/signup');
 const loginRoute = require('./routes/auth/login');
