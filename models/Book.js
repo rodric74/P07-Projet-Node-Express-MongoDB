39b69d9fb26d0ff9213e639c555e4828aad7fe45
rodric74 <rodric.rambosson@gmail.com>
2023-08-12 12:40:53 +0200

    Book Model OK 🎉

--- models/Book.js
+++ models/Book.js
@@ -1,9 +1,62 @@
 const mongoose = require('mongoose');
 
+
+const ratingSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: 'user',
+        required: true
+    },
+
+    grade: {
+        type: Number, 
+        required: true, 
+        min:0,
+        max:5
+    }
+
+});
+
 const bookSchema = new mongoose.Schema({
     userId:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+
+    title:{
+        type: String,
+        required: true
+    },
+
+    author:{
+        type: String,
+        required:true
+    },
+
+    imageUrl:{
+        type: String, 
+        required: true
+    },
+
+    year:{
+        type: Number,
+        required: true
+    },
+
+    genre:{
+        type: String,
+        required: true
+    },
+
+    rating: [ratingSchema],
+
+    averageRating:{
+        type: Number,
+        default: 0
     }
-})
+
+});
+
+
+module.exports = mongoose.model('Book', bookSchema)
