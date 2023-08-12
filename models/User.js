b7c0e50f2c468cbdeed3fa05ac0db5243df55c72
rodric74 <rodric.rambosson@gmail.com>
2023-08-12 09:20:36 +0200

    start work Users

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Hashage du mot de passe
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
