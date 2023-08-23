8e05338ce7d44fb0e1243123aebb834b1d9e6d74
rodric74 <rodric.rambosson@gmail.com>
2023-08-23 16:07:51 +0200

    rate lmiter à 5 minutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const limiter = require('../middleware/ratelimiter')
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+router.post('/signup', limiter, userController.signup);
+router.post('/login', limiter, userController.login);
 
 module.exports = router;
