76636ab86f0398c89e809a6d3990a7a9d695b297
rodric74 <rodric.rambosson@gmail.com>
2023-08-27 20:20:49 +0200

    multer -> imgOptim

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const multer = require('../middleware/imgOptim');
 
 
 
